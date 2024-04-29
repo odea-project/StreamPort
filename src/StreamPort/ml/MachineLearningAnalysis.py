6d0356a0a9384daf19a2359750f9599855686f65
AylinKantaroglu <aylinkantaroglu@gmail.com>
2024-04-29 13:05:20 +0200

    Changed the name

---
+++ src/StreamPort/ml/MachineLearningAnalysis.py
@@ -0,0 +1,22 @@
+from ..core.Analyses import Analyses
+
+class MachineLearningAnalysis(Analyses):
+
+    """
+    Represents MachineLearningAnalysis class that inherits from Analysis class
+
+    """
+
+    def __init__(self, name=None, replicate=None, blank=None, data=None):
+
+        """
+        Initializes the MachineLearningAnalysis instance
+
+        Args:
+            name (str): The name of the analysis.
+            replicate (str): The name of the replicate.
+            blank (str): The name of the blank.
+            data (list): The data of the analysis, which is a dict of one dimension numpy arrays.
+        """
+
+        super().__init__(name, replicate, blank, data)
