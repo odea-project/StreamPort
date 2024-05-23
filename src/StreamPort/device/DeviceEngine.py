c68e4764b53707fee3b4fa900eabf1529690cce8
SHA212 <sandeepharikrishnan@gmail.com>
2024-05-23 16:45:11 +0200

    Update DeviceEngine.py

--- src/StreamPort/device/DeviceEngine.py
+++ src/StreamPort/device/DeviceEngine.py
@@ -1,5 +1,5 @@
 from ..core.CoreEngine import CoreEngine
-
+import os
 
 class DeviceEngine(CoreEngine):
 
@@ -14,63 +14,66 @@ class DeviceEngine(CoreEngine):
         analyses (list, optional): The list of analyses. Instance or list of instances of DeviceAnalysis class.
         results (dict, optional): The dictionary of results.
 
+        _source (raw_str, optional): path to source directory of data/analysis. Can be an individual analysis or parent directory of multiple analyses.
+                                     source variable must be a raw string or exclude escape characters by using '\\' instad of '\'.
     Methods:
 
-        read_device_spectra(source(str, optional)) : 
+        read_device_spectra(self, source(raw_str, optional)) : 
                 Reads and arranges all available (pressure) spectra from a given path to a source directory.
                 If source not specified, searches cwd(current working directory) for compatible data.
                 Curves are grouped by unique Method ID and date of experiment.
 
-        plot_spectra() : 
+        plot_spectra(self) : 
                 Creates an interactive plot of (pressure) curves against time.
                 Unique for unique Method IDs.
                 To be expanded to handle other data(e.g. device conditions, curve features).
                 (Possible future upgrade)User can specify resolution and/or curve smoothing ratio.
 
-        add_features(features_list(str/list, optional)) :
+        add_features(self, features_list(str/list, optional)) :
                 Extracts features from curves and arranges data appropriately for further processing and ML operations.
                 If target features not specified, default list(mean, min, max, std) applied.
 
-        get_features() :
+        get_features(self) :
                 Returns/prints result dataset of extracted features over all samples.
 
-        drop_features(features_list(str/list of feature/column name(s), optional)) :
+        drop_features(self, features_list(str/list of feature/column name(s), optional)) :
                 Removes undesired features from prepared dataset. 
                 Essentially acts like df.drop(columns), where the columns are features to remove.
                 If argument unspecified, defaults to drop all features.
     """         
 
+    _source = r''
 
-    def __init__(self, headers=None, settings=None, analyses=None, results=None):
+    def __init__(self, headers=None, settings=None, analyses=None, results=None, source = os.getcwd()):
         
         super().__init__(headers, settings, analyses, results)
-
+        self._source = r'{}'.format(source)
     
 
-    def read_device_spectra(self, path = None):
+    def read_device_spectra(self, source):
 
         return()
     
 
 
-    def plot_spectra():
+    def plot_spectra(self):
 
         return()
     
 
 
-    def add_features():
+    def add_features(self):
 
         return()
     
 
 
-    def get_features():
+    def get_features(self):
 
         return()
     
 
 
-    def drop_features():
+    def drop_features(self):
 
         return()
